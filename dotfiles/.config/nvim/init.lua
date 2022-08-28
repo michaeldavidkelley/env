d36ef9755c2c6c2807641668d6ac2882d903274f
Michael Kelley <mkelley@digitalocean.com>
2022-08-28 14:22:51 -0400

    update dotfiles

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,3 +1,19 @@
+function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+function mapSilent(mode, lhs, rhs, opts)
+  local options = { silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  map(mode, lhs, rhs, opts)
+end
+
 vim.g.mapleader = ';'
 
 vim.opt.mouse = 'a'
@@ -9,3 +25,6 @@ vim.opt.listchars = { tab = '» ', trail = '·' }
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+
+mapSilent('n', '<Leader>s', ':w<CR>')
+mapSilent('n', '<Leader><Leader>', '<ESC>')
