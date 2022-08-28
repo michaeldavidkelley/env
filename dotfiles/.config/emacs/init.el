6c33c73a877e5a2b710911ffef192d84b524dea6
Michael Kelley <mkelley@digitalocean.com>
2022-08-28 16:30:37 -0400

    [emacs] plugins

--- dotfiles/.config/emacs/init.el
+++ dotfiles/.config/emacs/init.el
@@ -16,9 +16,32 @@
 (use-package lsp-mode
   :straight t)
 
+(use-package lsp-ui
+  :straight t
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-header t
+        lsp-ui-doc-include-signature t
+        lsp-ui-doc-max-width 80
+        lsp-ui-doc-max-height 20
+        lsp-ui-doc-use-childframe nil))
+
+(use-package protobuf-mode
+  :straight t)
+
 (use-package go-mode
   :straight t
-  :mode "\\.go\\'")
+  :after (lsp-mode)
+  :mode "\\.go\\'"
+  :hook (go-mode . (lambda ()
+                   (setq-local compile-command "go build -v && go test -race -cover -v && go vet")
+                   (setq-local compilation-read-command nil)
+                   (setq-local tab-width 2)
+                   (setq-local lsp-gopls-build-flags ["-tags=integration,sandbox"])
+                   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\vendor\\'")
+                   (add-hook 'before-save-hook #'lsp-format-buffer nil t)
+                   (add-hook 'before-save-hook #'lsp-organize-imports nil t)))
 
 (use-package evil
   :straight t
