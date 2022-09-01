06e285595f13cf789f62f02736bf2246e8751ba0
Michael Kelley <mkelley@digitalocean.com>
2022-09-01 12:47:04 -0400

    [emacs] packages

--- dotfiles/.config/emacs/init.el
+++ dotfiles/.config/emacs/init.el
@@ -13,35 +13,22 @@
 
 (straight-use-package 'use-package)
 
-(use-package lsp-mode
-  :straight t)
-
-(use-package lsp-ui
-  :straight t
-  :after lsp-mode
-  :commands lsp-ui-mode
-  :config
-  (setq lsp-ui-doc-header t
-        lsp-ui-doc-include-signature t
-        lsp-ui-doc-max-width 80
-        lsp-ui-doc-max-height 20
-        lsp-ui-doc-use-childframe nil))
-
 (use-package protobuf-mode
   :straight t)
 
 (use-package go-mode
   :straight t
-  :after (lsp-mode)
+  ;; :after (lsp-mode)
   :mode "\\.go\\'"
   :hook (go-mode . (lambda ()
                    (setq-local compile-command "go build -v && go test -race -cover -v && go vet")
                    (setq-local compilation-read-command nil)
-                   (setq-local tab-width 2)
-                   (setq-local lsp-gopls-build-flags ["-tags=integration,sandbox"])
-                   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\vendor\\'")
-                   (add-hook 'before-save-hook #'lsp-format-buffer nil t)
-                   (add-hook 'before-save-hook #'lsp-organize-imports nil t))))
+                   (setq-local tab-width 2))))
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
 
 (use-package evil
   :straight t
@@ -56,14 +43,4 @@
 	org-agenda-files '("~/org" "~/org/dailies" "~/org/1x1")
 	org-agenda-include-diary t))
 
-(use-package which-key
-  :straight t
-  :config (which-key-mode))
-
-(use-package vertico
-  :ensure t
-  :init
-  (vertico-mode))
-
 (setq tab-width 2)
-
